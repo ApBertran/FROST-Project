8bc56d950698c6d03ee476480927bae957b0c12b
ApBertran <ray200606@gmail.com>
2024-08-30 18:54:04 -0700

    minor logic error

--- Source/example/time_test.py
+++ Source/example/time_test.py
@@ -5,13 +5,16 @@ stopwatch_initial_time = localtime()
 while True:
     elapsed_time = (localtime().tm_hour - stopwatch_initial_time.tm_hour) * 3600 + (localtime().tm_min - stopwatch_initial_time.tm_min) * 60 + (localtime().tm_sec - stopwatch_initial_time.tm_sec)
     
+    # Calculate hours
     hours = str(elapsed_time // 3600)
 
-    minutes = str((elapsed_time - (elapsed_time // 3600) * 3600) // 60)
+    # Calculate minutes
+    minutes = str((elapsed_time - int(hours) * 3600) // 60)
     if int(minutes) < 10:
         minutes = '0' + minutes
     
-    seconds = str(elapsed_time - ((elapsed_time // 3600) * 3600) - (((elapsed_time // 3600) * 3600) // 60))
+    # Calculate seconds
+    seconds = str(elapsed_time - (int(hours) * 3600) - (int(minutes) * 60))
     if int(seconds) < 10:
         seconds = '0' + seconds
     
